37811f77c8257a4968883f2bce202345284a0906
Jonathan <jonathanatss@gmail.com>
2023-05-23 16:02:53 -0300

    cv-template update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -10,59 +10,50 @@ module.exports = {
     'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Java', 100],
+    ['Javascript', 100],
+    ['Python', 100],
+    ['Flutter', 100],
+    ['Selenium Webdriver', 95],
+    ['Robot Framework', 95],
+    ['Cypress', 100],
+    ['Scrum', 100],
+    ['Jira', 100],
+    ['Github', 100],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: 'QA Engineer',
+      period: 'april - currently',
+      skills: ['Sonarqube', 'Cypress', 'Selenium Webdriver', 'Javascript', 'C#', 'Jira', 'Scrum'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Working as a test engineer in the quality process of developing the Dell Sales Application (DSA) project, which is responsible for recording equipment and services sales. I am directly involved in planning tests involving the entire testing pyramid, from unit tests to end-to-end (e2e) tests.
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'QA Analyst',
+      period: 'october 2022 – march 2023',
+      skills: ['Flutter', 'Testes manuais', 'Scrum'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Working as QA Engineer in the development of the SASMDT Application - A smart mobile application for tracking drivers' trips, as well as checking telemetry during cargo transportation operations, in partnership with Seva Engenharia Eletrônica S.A. I am also responsible for unit testing, instrumented testing, and automated testing in Flutter/Dart.
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'Mobile Developer',
+      period: 'oct 2021 – sept 2022',
+      skills: ['Java', 'Automation Testing', 'Scrum', 'Git'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+The project I was part of was in partnership with Gertec, a Brazilian company responsible for a large part of payment terminals in Brazil. The technological solution of the project was the construction of a mobile application focused on quality, as well as the creation of code documentation and unit testing. The application aimed to test all interfaces and functionalities of payment terminals automatically, quickly, and objectively. The project was successfully completed, resulting in a reduction of the testing time of each terminal by an average of 4 hours.
 `
     }
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: 'Front End Development',
+      skills: ['React', 'NodeJS', `Javascript`, `CSS`, `HTML5`],
       contents: `
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
 `
     },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
